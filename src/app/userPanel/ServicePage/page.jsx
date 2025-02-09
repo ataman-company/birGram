384f2e5bbaea831e960d42184bafcee426b839ae
Ataman-Front-Mahdi <atamanorum@gmail.com@gmail.com>
2025-02-09 13:17:01 +0330

    css: userpanel css fixed

--- src/app/userPanel/ServicePage/page.jsx
+++ src/app/userPanel/ServicePage/page.jsx
@@ -114,7 +114,9 @@ function UserPanel() {
           </div>
         ))}
       </div>
-      <BottomNav />
+      <div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 z-[9999]">
+        <BottomNav />
+      </div>
     </div>
   );
 }
--- src/app/userPanel/transactions/page.jsx
+++ src/app/userPanel/transactions/page.jsx
@@ -88,7 +88,9 @@ const Transactions = () => {
         {/* Pass modal state to FilterModal */}
         <FilterModal open={isOpen} change={onOpenChange} />
 
-        <BottomNav />
+        <div className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-300 z-[9999]">
+          <BottomNav />
+        </div>
       </div>
     </>
   );
