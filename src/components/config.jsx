1052a2edf6ccf3288fb1b8d2ceab224a3dde1ad7
Ataman-Front-Mahdi <atamanorum@gmail.com@gmail.com>
2025-02-24 13:57:55 +0330

    final fix 2

--- src/components/config.jsx
+++ src/components/config.jsx
@@ -1,7 +1,7 @@
 const Config = {
-  // apiUrl: "https://gold.pronetwork.cloud/api/v1",
-  // baseUrl: "https://gold.pronetwork.cloud",
-  apiUrl: "http://192.168.1.21/api/v1",
-  baseUrl: "http://192.168.1.21",
+  apiUrl: "https://gold.pronetwork.cloud/api/v1",
+  baseUrl: "https://gold.pronetwork.cloud",
+  // apiUrl: "http://192.168.1.21/api/v1",
+  // baseUrl: "http://192.168.1.21",
 };
 export default Config;
